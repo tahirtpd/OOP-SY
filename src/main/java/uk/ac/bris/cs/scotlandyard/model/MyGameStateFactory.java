d5fc80e1f9b0c5289df32c728470bb3dc4468e30
tahirtpd <gm24543@bristol.ac.uk>
2025-03-04 17:09:48 +0000

    22 passed

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -1,6 +1,7 @@
 package uk.ac.bris.cs.scotlandyard.model;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import java.util.*;
 import javax.annotation.Nonnull;
@@ -25,15 +26,88 @@ public final class MyGameStateFactory implements Factory<GameState> {
 
 	private final class MyGameState implements GameState {
 
+		private GameSetup setup;
+		private ImmutableSet<Piece> remaining;
+		private ImmutableList<LogEntry> log; // holds travel log and counts Mr X's moves
+		private Player mrX;
+		private List<Player> detectives;
+		private ImmutableSet<Move> moves; // currently available moves
+		private ImmutableSet<Piece> winner;
+
+		private MyGameState(
+			final GameSetup setup, 
+			final ImmutableSet<Piece> remaining,
+			final ImmutableList<LogEntry> log, 
+			final Player mrX, 
+			final List<Player> detectives
+		) {
+			if (setup.moves.isEmpty()) throw new IllegalArgumentException("Moves cannot be empty");
+			if (setup.graph.nodes().isEmpty()) throw new IllegalArgumentException("Graph cannot be empty");
+			if (mrX == null) throw new NullPointerException("MrX cannot be null");
+			if (detectives.contains(null)) throw new NullPointerException("Detectives cannot contain null");
+
+			// temporary lists used by checks below
+			List<Piece> pieces = new ArrayList<>(Collections.emptyList());
+			List<Integer> locations = new ArrayList<>(Collections.emptyList());
+
+			for (Player player : detectives) {
+				if (pieces.contains(player.piece())) {
+					throw new IllegalArgumentException("Detectives cannot have duplicate pieces"); 
+				} else { 
+					pieces.add(player.piece()); 
+				}
+				
+				if (locations.contains(player.location())) {
+					throw new IllegalArgumentException("Multiple detectives cannot be at the same location");
+				} else { 
+					locations.add(player.location()); 
+				}
+				
+				if (player.has(Ticket.SECRET)) {
+					throw new IllegalArgumentException("Detectives cannot have secret tickets"); 
+				}
+
+				if (player.isDetective() && player.has(Ticket.DOUBLE)){
+					throw new IllegalArgumentException("Detectives cannot have double tickets");
+				}
+
+				if (player.piece().isMrX()) {
+					throw new IllegalArgumentException("There must only be one MrX");
+				}
+			}
+
+			// assign values
+			this.setup = setup;
+			this.remaining = remaining;
+			this.log = log;
+			this.mrX = mrX;
+			this.detectives = detectives;
+			this.moves = ImmutableSet.of();
+			this.winner = ImmutableSet.of();
+		}
+
 		@Nonnull @Override public GameSetup getSetup() { 
 			return setup; 
 		}
 
 		@Override public ImmutableSet<Piece> getPlayers() { 
-			return remaining; 
+			ImmutableSet.Builder<Piece> players = ImmutableSet.builder();
+			for (Player player : detectives) {
+				players.add(player.piece());
+			}
+			players.add(mrX.piece());
+			return players.build();
 		}
 		
 		@Override public GameState advance(Move move) { 
+			/*move.commencedBy()
+			move.tickets()
+			 
+			*/
+			if (!moves.contains(move)) throw new IllegalArgumentException("Illegal move: "+move);
+
+	
+
 			return null; 
 		}
 
@@ -49,6 +123,7 @@ public final class MyGameStateFactory implements Factory<GameState> {
 
 			return Optional.empty();
 		}
+
 		@Override public Optional<TicketBoard> getPlayerTickets(Piece piece) { 
 			// from ImmutableBoard.java : 79
 			if (piece.isDetective()) {
@@ -69,65 +144,105 @@ public final class MyGameStateFactory implements Factory<GameState> {
 		}
 		
 		@Override public ImmutableSet<Piece> getWinner() { 
-			return null; 
+			return winner;
 		}
 
-		@Override public ImmutableSet<Move> getAvailableMoves() { 
-			return moves; 
+		@Override public ImmutableSet<Move> getAvailableMoves() {
+			HashSet<Move> allMoves = new HashSet<>();
+
+			allMoves.addAll(makeSingleMoves(setup, detectives, mrX, mrX.location()));
+			allMoves.addAll(makeDoubleMoves(setup, detectives, mrX, mrX.location()));
+
+			// for (Player player : detectives) {
+			// 	allMoves.addAll(makeSingleMoves(setup, detectives, player, player.location()));
+			// }
+
+			return ImmutableSet.copyOf(allMoves);
+
 		}
 
-		private GameSetup setup;
-		private ImmutableSet<Piece> remaining;
-		private ImmutableList<LogEntry> log; // holds travel log and counts Mr X's moves
-		private Player mrX;
-		private List<Player> detectives;
-		private ImmutableSet<Move> moves; // currently available moves
-		private ImmutableSet<Piece> winner;
-
-		private MyGameState(
-			final GameSetup setup, 
-			final ImmutableSet<Piece> remaining,
-			final ImmutableList<LogEntry> log, 
-			final Player mrX, 
-			final List<Player> detectives
-		) {
-			if (setup.moves.isEmpty()) throw new IllegalArgumentException("Moves cannot be empty");
-			if (setup.graph.nodes().isEmpty()) throw new IllegalArgumentException("Graph cannot be empty");
-			if (mrX == null) throw new NullPointerException("MrX cannot be null");
-			if (detectives.contains(null)) throw new NullPointerException("Detectives cannot contain null");
-			
-			for (Player player : detectives) {
-				if (player.piece().isMrX()) { throw new IllegalArgumentException("There must only be one MrX"); }
-			}
-			
-			List<Piece> pieces = new ArrayList<>(Collections.emptyList());
-			List<Integer> locations = new ArrayList<>(Collections.emptyList());
-
-			for (Player player : detectives) {
-				if (pieces.contains(player.piece())) {
-					throw new IllegalArgumentException("Detectives cannot have duplicate pieces"); 
-				} else { 
-					pieces.add(player.piece()); 
+		private static Set<SingleMove> makeSingleMoves(GameSetup setup, List<Player> detectives, Player player, int source){
+			HashSet<SingleMove> moves = new HashSet<>();
+			destination_loop:
+			for (int destination : setup.graph.adjacentNodes(source)) {
+				for (Player p : detectives) {
+                    if (destination == p.location()) {
+                        continue destination_loop;
+                    }
 				}
 				
-				if (locations.contains(player.location())) {
-					throw new IllegalArgumentException("Multiple detectives cannot be at the same location");
-				} else { 
-					locations.add(player.location()); 
+				for (Transport t : setup.graph.edgeValueOrDefault(source, destination, ImmutableSet.of()) ) {
+					if (player.has(t.requiredTicket())) {
+						moves.add(new SingleMove(player.piece(), source, t.requiredTicket(), destination));
+					}
 				}
-				
-				if (player.has(Ticket.SECRET)) {
-					throw new IllegalArgumentException("Detectives cannot have secret tickets"); 
+
+				if (player.isMrX() && player.has(Ticket.SECRET)) {
+					moves.add(new SingleMove(player.piece(), source, Ticket.SECRET, destination));
 				}
 			}
-			
-			this.setup = setup;
-			this.remaining = remaining;
-			this.log = log;
-			this.mrX = mrX;
-			this.detectives = detectives;		
-		}
 
+            return moves;
+        }
+
+		private static Set<DoubleMove> makeDoubleMoves(GameSetup setup, List<Player> detectives, Player mrX, int source){
+			if (mrX.isDetective()) {
+				throw new IllegalArgumentException("Player is detective");
+			}
+
+			HashSet<DoubleMove> doubleMoves = new HashSet<>();
+			if (!mrX.has(Ticket.DOUBLE) || setup.moves.size() <= 1) {
+				return doubleMoves;
+			}
+
+			destination1Loop:
+			for (int destination1 : setup.graph.adjacentNodes(source)) {
+				for (Player p : detectives) {
+					if (destination1 == p.location()) {
+						continue destination1Loop;
+					}
+				}
+				
+				HashSet<SingleMove> singleMoves = new HashSet<>();
+				
+				for (Transport t : setup.graph.edgeValueOrDefault(source, destination1, ImmutableSet.of()) ) {
+					if (mrX.has(t.requiredTicket())) {
+						singleMoves.add(new SingleMove(mrX.piece(), source, t.requiredTicket(), destination1));
+					}
+				}
+				
+				for (SingleMove move: singleMoves) {
+					destination2Loop:
+					for (int destination2 : setup.graph.adjacentNodes(move.destination)) {
+						for (Player p : detectives) {
+							if (destination2 == p.location()) {
+								continue destination2Loop;
+							}
+						}
+						
+						for (Transport t : setup.graph.edgeValueOrDefault(destination1, destination2, ImmutableSet.of()) ) {
+							if (!(move.ticket == t.requiredTicket() && mrX.tickets().get(move.ticket) <= 1)) {
+								doubleMoves.add(new DoubleMove(mrX.piece(), source, move.ticket, move.destination, t.requiredTicket(), destination2));
+							}
+
+							if (mrX.has(Ticket.SECRET) && mrX.has(t.requiredTicket())) {
+								doubleMoves.add(new DoubleMove(mrX.piece(), source, Ticket.SECRET, move.destination, t.requiredTicket(), destination2));
+							}
+						}
+
+						if (mrX.has(Ticket.SECRET)) {
+							doubleMoves.add(new DoubleMove(mrX.piece(), source, move.ticket, move.destination, Ticket.SECRET, destination2));
+							
+							if (mrX.tickets().get(Ticket.SECRET) >= 2) {
+								doubleMoves.add(new DoubleMove(mrX.piece(), source, Ticket.SECRET, move.destination, Ticket.SECRET, destination2));
+							}
+						}
+					}					
+				}
+			}
+
+            return doubleMoves;
+        }
 	}
 
 }
